b73aa2c40df58d5762f11567c7377d02fe0f158e
伍润威 <rottenwu@tmgworldwide.com>
2020-05-14 08:13:13 +0800

    :arrow_up: typescript升级到v3.9.2版本 :bug:删掉错误样例

--- src/api/wxfetch.ts
+++ src/api/wxfetch.ts
@@ -23,7 +23,7 @@ function createRequest(baseUrl) {
   return request;
 }
 
-let ajax:any = createRequest('http://47.114.121.242')
+let ajax:any = createRequest('http://www.example.com')
 /** request拦截器 */
 ajax.interceptors.request.use((url, options) => {
   console.log(options)
